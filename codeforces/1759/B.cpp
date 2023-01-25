6a1e4971d31af4c6f4dd10883ee6fa7dea50f503
mtheggi <mtheggi@gmail.com>
2023-01-25 11:44:00 +0200

    Add solution for problem `B - Lost Permutation` Link: https://codeforces.com/contest/1759/problem/B Tags: math, *800 Ref: https://codeforces.com/contest/1759/submission/190468049

---
+++ codeforces/1759/B.cpp
@@ -0,0 +1,85 @@
+// Just Keep it up, long way to go.
+#include<bits/stdc++.h>
+using namespace std;
+// Macros
+#define RunIt ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define lld long double
+#define el endl
+#define re return
+#define YES cout << "Yes\n"
+#define NO cout << "No\n"
+#define f first
+#define s second
+#define len(z) (int)z.size()
+#define pb push_back
+#define eb emplace_back
+#define popcnt(a) __builtin_popcount(a)
+#define all(a) a.begin(),a.end()
+#define allr(a) a.rbegin(),a.rend()
+// Debug
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " :" << x << endl;
+#define debugv(v) cerr << #v << endl;  for(auto & i : v ) cerr << i << " " ; cerr << endl;
+#define ShowTime cerr << "Code Takes  : " << (float) clock() / CLOCKS_PER_SEC << setprecision(7) <<  fixed << " Secs to run." << endl;
+#else
+#define debug(x)
+#define debugv(v)
+#define ShowTime
+#endif
+// constants
+const int MOD = 1e9+7;
+const double EPS = 1e-9;
+const int N = 30 ;
+// used Funcions
+ll GCD(ll a, ll b) { return (a) ? GCD(b % a, a) : b; }
+ll LCM(ll a, ll b) { return a * b / GCD(a, b); }
+ll fastpow(ll b, ll p) { if (!p) return 1; ll ret = fastpow(b, p >> 1); ret *= ret; if (p & 1) ret *= b; return ret; }
+int dx[8] ={0 ,0 , 1, -1 , 1, -1 , 1 , -1  };
+int dy[8] ={1,-1, 0 , 0, 1 , -1 , -1 , 1 };
+void EZ() {
+    int n ,s ;
+    cin >> n >>s ;
+    vector<bool> v(100, 0 );
+    int mx = -1 ;
+
+    for(int i =0 ; i < n ; i++) {
+        int a ; cin >> a ;
+         v[a] =1 ;
+        mx=  max(a, mx );
+    }
+    int sum =0;
+    bool f =0;
+    for(int i =1 ;i <= 100 ; i++) {
+        if(!v[i]){
+            sum+=i;
+            v[i]=1 ;
+        }
+        if(sum== s ) {f=1 ; break; mx = max(i, mx ); }
+        else if(sum>s ) break;
+    }
+    if(f)
+     for(int i =1 ; i <=mx ; i++) if(!v[i]) {f=0; break;}
+
+    cout << (f?"YES":"NO") << endl; 
+
+}
+int main() {
+    RunIt;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    // freopen("Error", "w", stderr);
+#endif
+
+    int tc =1 ;
+    cin >> tc;
+    while(tc--){
+        EZ();
+    }
+    ShowTime
+    return 0 ;
+}
+
+
+
