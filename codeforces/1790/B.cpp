5c398bf7b89afecea4eb2c47c2ae6fa4d5aaa9a4
mtheggi <mtheggi@gmail.com>
2023-01-27 17:02:00 +0200

    Add solution for problem `B - Taisia and Dice` Link: https://codeforces.com/contest/1790/problem/B Tags: greedy, greedy, math, *800 Ref: https://codeforces.com/contest/1790/submission/190806586

---
+++ codeforces/1790/B.cpp
@@ -0,0 +1,80 @@
+// Just Keep it up, long way to go.
+#include<bits/stdc++.h>
+using namespace std;
+// Macros
+#define RunIt ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define lld long double
+#define el endl
+#define re return
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define f first
+#define s second
+#define len(z) (int)z.size()
+#define pb push_back
+#define eb emplace_back
+#define popcnt(a) __builtin_popcount(a)
+#define all(a) a.begin(),a.end()
+#define allr(a) a.rbegin(),a.rend()
+// Debug
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " :" << x << endl;
+#define debugv(v) cerr << #v << endl;  for(auto & i : v ) cerr << i << " " ; cerr << endl;
+#define ShowTime cerr << "Code Takes  : " << (float) clock() / CLOCKS_PER_SEC << setprecision(7) <<  fixed << " Secs to run." << endl;
+#else
+#define debug(x)
+#define debugv(v)
+#define ShowTime
+#endif
+// constants
+const int MOD = 1e9+7;
+const double EPS = 1e-9;
+const int N = 1e6 ;
+const int offset = 150000;
+// used Funcions
+ll GCD(ll a, ll b) { return (a) ? GCD(b % a, a) : b; }
+ll LCM(ll a, ll b) { return a * b / GCD(a, b); }
+ll fastpow(ll b, ll p) { if (!p) return 1; ll ret = fastpow(b, p >> 1); ret *= ret; if (p & 1) ret *= b; return ret; }
+
+void EZ() {
+    int n, t, r, mx ;
+    cin >> n >> t >> r;
+    mx = t-r ;
+    vector<int> ans(n+1 , 0 );
+    ans[1] = mx;
+    bool z =0;
+    while(r>0){
+        for(int i =2;i <=n ;i++) {
+            if(ans[i] < mx ) {
+                ans[i]++;
+                r--;
+            }
+            if(r <=0) {z =1 ;break;}
+        }
+        if(z) break;
+    }
+    for(int i =1 ; i<=n ;i++) cout << ans[i] << " " ;
+    cout << endl;
+
+}
+
+int main() {
+    RunIt;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    // freopen("Error", "w", stderr);
+#endif
+
+    int tc =1 ;
+    cin >> tc;
+    while(tc-- ){
+        EZ();
+    }
+
+    ShowTime
+    return 0 ;
+}
+
+
