8339e9e437250bbd82b3e078d9ed030f4fe1cc93
mtheggi <mtheggi@gmail.com>
2023-01-29 11:05:00 +0200

    Add solution for problem `C - Premutation` Link: https://codeforces.com/contest/1790/problem/C Tags: brute force, implementation, math, *1000 Ref: https://codeforces.com/contest/1790/submission/191071116

---
+++ codeforces/1790/C.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Macros
+#define RunIt ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define lld long double
+#define el endl
+#define re return
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define f first
+#define s second
+#define len(z) (int)z.size()
+#define pb push_back
+#define eb emplace_back
+#define popcnt(a) __builtin_popcount(a)
+#define all(a) a.begin(),a.end()
+#define allr(a) a.rbegin(),a.rend()
+// Debug
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " :" << x << endl;
+#define debugv(v) cerr << #v << endl;  for(auto & i : v ) cerr << i << " " ; cerr << endl;
+#define ShowTime cerr << "Code Takes  : " << (float) clock() / CLOCKS_PER_SEC << setprecision(7) <<  fixed << " Secs to run." << endl;
+#else
+#define debug(x)
+#define debugv(v)
+#define ShowTime
+#endif
+// constants
+const int MOD = 1e9+7;
+const double EPS = 1e-9;
+const int N = 1e6 ;
+const int offset = 150000;
+// used Funcions
+ll GCD(ll a, ll b) { return (a) ? GCD(b % a, a) : b; }
+ll LCM(ll a, ll b) { return a * b / GCD(a, b); }
+ll fastpow(ll b, ll p) { if (!p) return 1; ll ret = fastpow(b, p >> 1); ret *= ret; if (p & 1) ret *= b; return ret; }
+
+
+void EZ(){
+	int n ; cin >> n ;
+    vector<vector<int>> v(n , vector<int>(n)); 
+    for(int i =0 ;i <n ; i++)
+        for(int j =0 ;j < n-1 ; j++) {
+            cin >> v[i][j]; 
+        } 
+    map<int ,int > detected ; 
+    vector<int> ans(n);
+    vector<bool> vis(N , 0 );
+    for(int j = 0 ; j<n-1 ; j++ ) {
+        set<int> st ; 
+        for(int i =0 ;i < n ;i++){
+            st.insert(v[i][j]); 
+            detected[v[i][j]]++; 
+        }
+        auto it = st.begin(); 
+        int first = *it; it++; 
+        int second = *it;   
+
+        if( detected[first] == detected[second] ){
+                if(!vis[first])  ans[j+1] =first ;
+                if(!vis[second])  ans[j+1] =second  ;
+
+        }else{
+            if(detected[first] < detected[second]) swap(first, second); 
+
+            ans[j] =  first;
+            ans[j+1] = second ;
+            
+        } 
+        vis[first] =1 ; 
+        vis[second] =1 ;
+}
+        for(auto i : ans) cout << i << " " ; 
+            cout << el; 
+        
+}
+
+#define ll long long
+int main()
+{
+    RunIt; 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "rt", stdin);
+#endif
+#ifndef ONLINE_JUDGE
+    freopen("Output.txt", "wt", stdout);
+#endif
+    int tc =1 ;
+ 
+
+    cin >> tc;  
+    while(tc--){
+    	EZ(); 
+    }
+	return 0;
+}
