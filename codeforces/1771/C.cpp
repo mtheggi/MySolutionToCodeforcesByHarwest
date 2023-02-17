bbb422e8880764121c077df3f25118fef4b5639c
mtheggi <mtheggi@gmail.com>
2023-02-17 13:05:00 +0200

    Add solution for problem `C - Hossam and Trainees` Link: https://codeforces.com/contest/1771/problem/C Tags: greedy, math, number theory, *1600 Ref: https://codeforces.com/contest/1771/submission/193979992

---
+++ codeforces/1771/C.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Macros
+#define RunIt ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define lld long double
+#define el endl
+#define re return
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define f first
+#define s second
+#define len(z) (int)z.size()
+#define pb push_back
+#define eb emplace_back
+#define popcnt(a) __builtin_popcount(a)
+#define all(a) a.begin(),a.end()
+#define allr(a) a.rbegin(),a.rend()
+// Debug
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " :" << x << endl;
+#define debugv(v) cerr << #v << endl;  for(auto & i : v ) cerr << i << " " ; cerr << endl;
+#define ShowTime cerr << "Code Takes  : " << (float) clock() / CLOCKS_PER_SEC << setprecision(7) <<  fixed << " Secs to run." << endl;
+#else
+#define debug(x)
+#define debugv(v)
+#define ShowTime
+#endif
+// constants
+const int MOD = 1e9+7;
+const double EPS = 1e-9;
+const int N = 31622 + 20  ,  M = 2e5 + 10  ;
+// used Funcions
+ll GCD(ll a, ll b) { return (a) ? GCD(b % a, a) : b; }
+ll LCM(ll a, ll b) { return a * b / GCD(a, b); }
+ll fastpow(ll b, ll p) { if (!p) return 1; ll ret = fastpow(b, p >> 1); ret *= ret; if (p & 1) ret *= b; return ret; }
+int spf[N]; // smallest Prime Factor
+//vector<int> primes[N];
+bool isprime[N];
+void sieve()
+{ // O(n * (log(log(n)))
+    memset(isprime ,true , sizeof isprime);
+    isprime[0] = isprime[1] = false ;
+    for (int i = 2; i * i < N; i++) for (int j = i+i; j < N; j += i) isprime[j] =false ;
+}
+vector<int> p;
+int  x, y;
+void EZ() {
+    cin >> x ;
+    vector<ll> a ;
+    map<int ,int > freqfact;
+    for(int i  = 1; i <= x ; i++) { cin >> y ; a.pb(y);}
+    for(int i =0 ;i < x ; i++) {
+        y = a[i];
+        if( y == 1 ) continue;
+        for(auto i : p ){
+            if(i < y){
+                if(!(y%i)){
+                    freqfact[i]++;
+                    while(y%i==0) y/=i;
+                }
+
+            }else break;
+        }
+        if(y >1) freqfact[y]++;
+    }
+    for(auto i : freqfact) if(i.s >=2 ) {YES;re;}
+    NO;
+}
+
+int main() {
+    RunIt;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    // freopen("Error", "w", stderr);
+#endif
+    int tc =1 ; cin >> tc;
+    sieve();
+    cerr << isprime[1] << el;
+    for(int i = 1 ; i <N ; i++) if(isprime[i]) p.pb(i);
+    cerr << p.size() << el ;
+    while(tc--){
+        EZ();
+    }
+
+    ShowTime
+    return 0 ;
+}
+
