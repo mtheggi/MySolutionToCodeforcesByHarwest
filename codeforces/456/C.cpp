32cee0237c76770ba344b35304c8c67e42bc8ddb
mtheggi <mtheggi@gmail.com>
2023-02-18 23:01:00 +0200

    Add solution for problem `C - Boredom` Link: https://codeforces.com/contest/456/problem/C Tags: dp, *1500 Ref: https://codeforces.com/contest/456/submission/194165167

---
+++ codeforces/456/C.cpp
@@ -0,0 +1,82 @@
+// Just Keep it up, long way to go.
+#include<bits/stdc++.h>
+using namespace std;
+// Macros
+#define RunIt ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define lld long double
+#define el endl
+#define re return
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define f first
+#define s second
+#define len(z) (int)z.size()
+#define pb push_back
+#define eb emplace_back
+#define popcnt(a) __builtin_popcount(a)
+#define all(a) a.begin(),a.end()
+#define allr(a) a.rbegin(),a.rend()
+// Debug
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " :" << x << endl;
+#define debugv(v) cerr << #v << endl;  for(auto & i : v ) cerr << i << " " ; cerr << endl;
+#define ShowTime cerr << "Code Takes  : " << (float) clock() / CLOCKS_PER_SEC << setprecision(7) <<  fixed << " Secs to run." << endl;
+#else
+#define debug(x)
+#define debugv(v)
+#define ShowTime
+#endif
+// constants
+const int MOD = 1e9+7;
+const double EPS = 1e-9;
+const int N = 1e5 +10  ;
+// used Funcions
+ll GCD(ll a, ll b) { return (a) ? GCD(b % a, a) : b; }
+ll LCM(ll a, ll b) { return a * b / GCD(a, b); }
+ll fastpow(ll b, ll p) { if (!p) return 1; ll ret = fastpow(b, p >> 1); ret *= ret; if (p & 1) ret *= b; return ret; }
+int n ;
+vector<int> v;
+ll freq[N];
+ll dp[N];
+int mx = -1 ;
+ll findmx(ll val  ) {
+    if(val > mx ) return 0;
+    if(dp[val] != -1 ) return dp[val];
+    ll t = val *freq[val] + findmx(val+2);
+    ll l = findmx(val +1 );
+
+    return dp[val] = max(t , l );
+
+}
+
+
+void EZ(){
+    cin >>  n;
+    memset(dp , -1 , sizeof dp );
+    for(int i =1 ; i <=n ; i++){ int a ; cin >> a ;mx=max(mx , a)  ;  freq[a]++;  }
+    cout << findmx(1) << el;
+
+}
+
+
+int main() {
+
+    RunIt;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    // freopen("Error", "w", stderr);
+#endif
+
+    int tc =1 ;
+//     cin >> tc;
+    while(tc--){
+        EZ();
+    }
+
+    ShowTime
+    return 0 ;
+}
+
+
