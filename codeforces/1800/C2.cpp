2eb23ac8b1de12df13c2a6f71179fb0bde63eb06
mtheggi <mtheggi@gmail.com>
2023-03-04 16:02:00 +0200

    Add solution for problem `C2 - Powering the Hero (hard version)` Link: https://codeforces.com/contest/1800/problem/C2 Tags: data structures, greedy Ref: https://codeforces.com/contest/1800/submission/195977781

---
+++ codeforces/1800/C2.cpp
@@ -0,0 +1,82 @@
+// Just Keep it up, long way to go.
+#include<bits/stdc++.h>
+using namespace std;
+// Macros
+#define RunIt ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define lld long double
+#define el endl
+#define re return
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define f first
+#define s second
+#define len(z) (int)z.size()
+#define pb push_back
+#define eb emplace_back
+#define popcnt(a) __builtin_popcount(a)
+#define all(a) a.begin(),a.end()
+#define allr(a) a.rbegin(),a.rend()
+// Debug
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " :" << x << endl;
+#define debugv(v) cerr << #v << endl;  for(auto & i : v ) cerr << i << " " ; cerr << endl;
+#define ShowTime cerr << "Code Takes  : " << (float) clock() / CLOCKS_PER_SEC << setprecision(7) <<  fixed << " Secs to run." << endl;
+#else
+#define debug(x)
+#define debugv(v)
+#define ShowTime
+#endif
+// constants
+const int MOD = 998244353;
+const double EPS = 1e-9;
+const int N = 1e6 ;
+// used Funcions
+ll GCD(ll a, ll b) { return (a) ? GCD(b % a, a) : b; }
+ll LCM(ll a, ll b) { return a * b / GCD(a, b); }
+ll fastpow(ll b, ll p) { if (!p) return 1; ll ret = fastpow(b, p >> 1); ret *= ret; if (p & 1) ret *= b; return ret; }
+
+
+void EZ(){
+    int n;  cin >> n ;
+    vector<int> v(n) ;
+    for(int i  =0 ; i < n ; i++) {cin >> v[i] ; }
+    priority_queue<int > pq ;
+    bool zeroend =0 ;
+    ll ans =0;
+    int zerocnt =0;
+    for(int i =0 ;i < n ; i++) {
+        if(v[i] !=0 ){
+            pq.push( v[i]);
+        }else{
+            if(pq.size()) {
+                ans+= (pq.top());
+                pq.pop();
+            }
+        }
+    }
+    cout << ans << el;
+
+
+}
+
+
+int main() {
+
+    RunIt;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    // freopen("Error", "w", stderr);
+#endif
+
+    int tc =1 ;
+    cin >> tc;
+    while(tc--){
+        EZ();
+    }
+
+    ShowTime
+    return 0 ;
+}
+
